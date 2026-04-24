29c8260600b29e74c9b858954acb684f810944ff
ompatel303 <72147525+ompatel303@users.noreply.github.com>
2026-04-24 00:28:04 -0400

    Initial commit for data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,6 @@
 import sys
 import logging
-import src.logger
-logging.basicConfig(level=logging.INFO)
+from src.logger import logging
 
 def error_message_details(error, error_details:sys):
     _,_,exc_tb = error_details.exc_info()
