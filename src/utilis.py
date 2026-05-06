58e60fe37e569e58fc33007d3a83943aa5b275b9
ompatel303 <72147525+ompatel303@users.noreply.github.com>
2026-05-06 15:43:11 -0400

    Hyperparameter Tuning Implementation

--- src/utilis.py
+++ src/utilis.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,7 +22,7 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
@@ -29,6 +30,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
             model = list(models.values())[i]
 
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
